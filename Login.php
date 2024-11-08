dd04e70a60e5c7c6e9fb80ef77ecdb8bce38ab16
MilosJovic23 <milos.mj992@gmail.com>
2024-11-08 14:45:03 +0100

    * small changes

--- Login.php
+++ Login.php
@@ -5,7 +5,7 @@
         <title>Domaci php</title>
     </head>
     <body>
-    <form action="Domaci.php" method="POST">
+    <form action="modeli/PhpForLogin.php" method="POST">
         <input type="text" name="email" placeholder="unesite email" required>
         <input type="password" name="password" placeholder="uneiste lozinku" required>
         <button type="submit">Registruj me</button>
